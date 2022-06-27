3902b6347fa97b2b66d4ff534e132669942f0b27
Go Iwai <go.iwai@kek.jp>
2022-06-27 15:44:51 +0900

    removed unnecessary lines

--- oop/compiler/prototypesdef.h
+++ oop/compiler/prototypesdef.h
@@ -5,14 +5,6 @@
 //   type of return
 //   function arguments
 // but not give the implementation like specific processing in the function.
-// The file gives function usage.
-#include "prototypesdef.h"
-
-    // Instead of including the header file above,
-    // these two lines also allow to use the function in the program
-    // extern void echo(const char* message);
-    // extern int add(int x, int y);
-    void
-    echo(const char *message);
+void echo(const char *message);
 int add(int x, int y);
 #endif
